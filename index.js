8b71f288711142dc336c1e9c85dc7eac434b10ab
unknown <soniprince142@gmail.com>
2023-04-03 17:25:04 +0530

    deploy check

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const routes = require('./routes/route')
+const routes = require('./src/routes/route')
 
 const app = express()
 app.use(express.json())
